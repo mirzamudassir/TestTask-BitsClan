66d7e9d098024d0d5a166ad56773d013527b0e6b
mirzamudassir <mirzamudassir52@hotmail.com>
2022-09-21 07:11:47 +0500

    minor updates

--- readme.txt
+++ readme.txt
@@ -1,2 +1,7 @@
-Laravel v9.30.1 
-PHP v8.1.5
+Laravel Version : v9.30.1 
+PHP Version:  v8.1.5
+
+Admin Credentials:
+
+email: mudassir@email.com
+password: 12345678
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\ImageController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
 
 //login route
